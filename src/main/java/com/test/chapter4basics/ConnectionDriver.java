117e8f2edf4cd14443481592fe57786100face34
woyaodeyangguang <603819372@qq.com>
2017-11-29 22:24:11 +0800

    add connection driver and connection pool

---
+++ src/main/java/com/test/chapter4basics/ConnectionDriver.java
@@ -0,0 +1,30 @@
+package com.test.chapter4basics;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 通过动态代理来构造Connection.
+ */
+public class ConnectionDriver {
+
+
+  static class ConnectionHanlder implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+      if(method.getName().equals("commit")) {
+        TimeUnit.MILLISECONDS.sleep(100);
+      }
+      return null;
+    }
+  }
+
+  // 创建一个Connection的代理,在commit时休眠100毫秒
+  public static final Connection createConnection() {
+    return (Connection) Proxy.newProxyInstance(ConnectionDriver.class.getClassLoader(),
+        new Class<?>[]{Connection.class}, new ConnectionHanlder());
+  }
+}
---
+++ src/main/java/com/test/chapter4basics/ConnectionPoll.java
@@ -0,0 +1,58 @@
+package com.test.chapter4basics;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+/**
+ * 简单的数据库连接池
+ */
+public class ConnectionPoll {
+  private LinkedList<Connection> pool = new LinkedList<>();
+
+  public ConnectionPoll(int initialSize) {
+    if(initialSize > 0) {
+      for (int i = 0; i < initialSize; i++) {
+        pool.add(ConnectionDriver.createConnection());
+      }
+    }
+  }
+
+  public void releaseConnection(Connection connection) {
+    if(connection != null) {
+      synchronized (pool) {
+        // 连接释放后需要进行通知,这样其他消费者才能够感知到连接池已经归还了一个连接
+        pool.addLast(connection);
+        pool.notifyAll();
+      }
+    }
+  }
+
+  // 在mills内无法获取连接,将返回null
+  public Connection fectConnection(long mills) throws InterruptedException {
+    synchronized (pool) {
+      if(mills <= 0) {
+        while(pool.isEmpty()) {
+          pool.wait();
+        }
+        return pool.removeFirst();
+      } else {
+        long future = System.currentTimeMillis() + mills;
+        long remaing = mills;
+        while(pool.isEmpty() && remaing > 0) {
+          pool.wait(remaing);
+          remaing = future - System.currentTimeMillis();
+        }
+      }
+      Connection result = null;
+      if(!pool.isEmpty()) {
+        result = pool.removeFirst();
+      }
+      return result;
+    }
+  }
+
+
+
+
+
+}

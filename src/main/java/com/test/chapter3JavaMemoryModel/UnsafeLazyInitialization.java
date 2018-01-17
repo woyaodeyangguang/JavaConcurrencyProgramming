cf7652206ef90d87e1d82b18726a5149a57fca7e
woyaodeyangguang <603819372@qq.com>
2018-01-17 21:49:13 +0800

    add test case.

--- src/main/java/com/test/chapter3JavaMemoryModel/UnsafeLazyInitialization.java
+++ src/main/java/com/test/chapter3JavaMemoryModel/UnsafeLazyInitialization.java
@@ -7,9 +7,26 @@ public class UnsafeLazyInitialization {
   private static Instance instance;
 
   public static Instance getInstance() {
+    try {
+      Thread.currentThread().sleep(100);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     if(instance == null) {
       instance = new Instance();
     }
     return instance;
+  }
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          UnsafeLazyInitialization.getInstance();
+        }
+      });
+      thread.start();
+    }
   }
 }
